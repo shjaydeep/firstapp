2bbf9b8407c0d1a8b417762b20056881f4d42313
shjaydeep <jaydeep@siasoftwares.com>
2020-12-24 17:11:39 +0530

    pull request added

--- src/app/pages/pullrequest/pullrequest.component.ts
+++ src/app/pages/pullrequest/pullrequest.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FirepullService } from 'src/app/services/firepull.service';
 import { Router } from '@angular/router';
+import { element } from 'protractor';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 @Component({
   selector: 'app-pullrequest',
@@ -10,29 +12,31 @@ import { Router } from '@angular/router';
 })
 export class PullrequestComponent implements OnInit {
   listarray: any = [];
+
   constructor(private firepullservice: FirepullService, private router: Router) { }
 
   ngOnInit() {
-    this.firepullservice.getToDoList().snapshotChanges()
+
+    this.listarray = this.firepullservice.getToDoList().snapshotChanges()
     .subscribe(item => {
       this.listarray = [];
       item.forEach(element => {
         var x = element.payload.toJSON();
-        x["$key"] = element.key;
+       // console.log(x);
+        // x["$key"] = element.key;
         this.listarray.push(x);
       })
-
-      //sort array isChecked false  -> true and all
-        this.listarray.sort((a,b) => {
-          return a.isChecked - b.isChecked;
-        })
-    });
+   // console.log(this.toDoService.getToDoList());
+   console.log(this.listarray);
+    })
 
   }
-  onAdd(itemTitle) {
 
-    this.firepullservice.addTitle(itemTitle.value, this.itemlist);
-    itemTitle.value = null;
+  onAdd(pullname, projectname) {
+
+    this.firepullservice.addTitle(pullname.value, projectname.value, this.listarray);
+    pullname.value = null;
+    projectname.value = null;
     this.router.navigate(['/lists']);
   }
 
@@ -46,3 +50,58 @@ export class PullrequestComponent implements OnInit {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this.listarray = this.firepullservice.getToDoList().snapshotChanges()
+// .subscribe(item => {
+//   this.listarray = [];
+//   item.forEach(element => {
+//     var x = element.payload.toJSON();
+//     x["$key"] = element.key;
+//     this.listarray.push(x);
+//     console.log(this.listarray[1]);
+//     this.listarray = this.listarray[1];
+//     //this.listarray = this.listarray[1];
+
+//   })
+
+// // console.log(this.toDoService.getToDoList());
+
+
+// })
+
+
+// // this.firepullservice.getToDoList().snapshotChanges()
+// // .subscribe(item => {
+// //   this.listarray = [];
+// //   item.forEach(element => {
+// //     var x = element.payload.toJSON();
+// //     x["$key"] = element.key;
+// //     this.listarray.push(x);
+
+// //   })
+
+// //   //sort array isChecked false  -> true and all
+// //     this.listarray.sort((a,b) => {
+// //       return a.isChecked - b.isChecked;
+// //     })
+// // });
--- src/app/services/firepull.service.ts
+++ src/app/services/firepull.service.ts
@@ -15,19 +15,21 @@ export class FirepullService {
   getToDoList() {
     const uid = localStorage.getItem("uid");
     const t = localStorage.getItem("title");
-    this.toDoList = this.firebasedb.list('users/'+uid+'/CheckList/'+t);
+    this.toDoList = this.firebasedb.list('users/'+uid+'/CheckList/'+t+'/task_list/');
+
     return this.toDoList;
   }
 
-  // addTitle(title: string, itemlist: string[]) {
-  //   const uid = localStorage.getItem("uid");
-  //   const t = localStorage.getItem("title");
-  //   this.toDoList = this.firebasedb.list('users/'+uid+'/CheckList/'+t);
-  //   this.toDoList.set(title,{
-  //     task_list: itemlist,
-  //     title: title
-  //   });
-  // }
+  addTitle(pullname: string, projectname: string, itemlist: string[]) {
+    const uid = localStorage.getItem("uid");
+    const t = localStorage.getItem("title");
+    this.toDoList = this.firebasedb.list('users/'+uid+'/CheckList/'+t+'/PullRequest/');
+    this.toDoList.set(pullname,{
+     project_name:  projectname,
+     pullRequest_name: pullname,
+     pullRequest_CheckList: itemlist
+    });
+  }
 
   checkOrUnCheckTitle($key: string, flag: boolean) {
     this.toDoList.update($key, { isChecked: flag });
